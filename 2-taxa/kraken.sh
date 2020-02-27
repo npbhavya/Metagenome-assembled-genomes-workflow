d420aab8188022f76269a31cb029201753d11e50
Bhavya Papudeshi <npbhavya13@gmail.com>
2020-02-27 08:45:40 -0500

    Update kraken.sh

--- 2-taxa/kraken.sh
+++ 2-taxa/kraken.sh
@@ -10,21 +10,21 @@ cd PWDHERE
 module load kraken/2.0.8
 
 #Code for making a list of all the reads in the reads directory
-reads=`ls reads | grep "1.fastq" | sed 's/_1.fastq//g'`
+reads=`ls 1-reads | grep "1.fastq" | sed 's/_1.fastq//g'`
 
 for f in $reads
 do 
-	kraken2 --reload --db $KRAKEN_DB --paired reads/"$f"_1.fastq reads/"$f"_2.fastq --threads 1 --use-names --report taxa/"$f"_kraken_report --output taxa/"$f"_kraken.out
+	kraken2 --reload --db $KRAKEN_DB --paired 1-reads/"$f"_1.fastq 1-reads/"$f"_2.fastq --threads 1 --use-names --report 2-taxa/"$f"_kraken_report --output 2-taxa/"$f"_kraken.out
 done
 
 #moving the report to a new directory to generate one output 
-mkdir taxa/kraken_report
-mv taxa/*_kraken_report taxa/kraken_report/.
+mkdir 2-taxa/kraken_report
+mv 2-taxa/*_kraken_report taxa/kraken_report/.
 
 module unload python 
 module load python/3.6.8 
 export PATH=PWDHEREscripts:$PATH
 
-python scripts/kraken-multiple-taxa.py -d taxa/kraken_report/ -r F -c 2 -o taxa/kraken-report-final
+python scripts/kraken-multiple-taxa.py -d 2-taxa/kraken_report/ -r F -c 2 -o 2-taxa/kraken-report-final
 #just changing the format to a csv
-sed -e "s/\[//g;s/\]//g;s/'//g;s|\t|,|g" taxa/kraken-report-final  >taxa/kraken_report-final.csv
+sed -e "s/\[//g;s/\]//g;s/'//g;s|\t|,|g" 2-taxa/kraken-report-final  >2-taxa/kraken_report-final.csv
