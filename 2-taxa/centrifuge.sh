78b92a36a796f44e677909e61afde0128c81ad52
Bhavya Papudeshi <npbhavya13@gmail.com>
2020-02-27 08:43:14 -0500

    Update centrifuge.sh

--- 2-taxa/centrifuge.sh
+++ 2-taxa/centrifuge.sh
@@ -10,11 +10,11 @@ cd PWDHERE
 module load centrifuge/1.0.3
 
 #Code for making a list of all the reads in the reads directory
-reads=`ls reads | grep "1.fastq" | sed 's/_1.fastq//g'`
+reads=`ls 1-reads | grep "1.fastq" | sed 's/_1.fastq//g'`
 
 for f in $reads
 do 
-	centrifuge -x $CENTRIFUGE_INDEX -1 reads/"$f"_1.fastq -2 reads/"$f"_2.fastq > taxa/"$f".centrifuge.out
-	mv centrifuge_report.tsv taxa/"$f"_centrifige_report.tsv
+	centrifuge -x $CENTRIFUGE_INDEX -1 1-reads/"$f"_1.fastq -2 1-reads/"$f"_2.fastq > 2-taxa/"$f".centrifuge.out
+	mv centrifuge_report.tsv 2-taxa/"$f"_centrifige_report.tsv
 done
 
