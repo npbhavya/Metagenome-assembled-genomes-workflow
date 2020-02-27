1641385b62fa3b61ab12442e306c99028de06d6e
Bhavya Papudeshi <npbhavya13@gmail.com>
2020-02-27 08:44:38 -0500

    Update focus.sh

--- 2-taxa/focus.sh
+++ 2-taxa/focus.sh
@@ -9,16 +9,16 @@ cd PWDHERE
 
 #pairing the reads 
 export PATH=PWDHEREscripts:$PATH
-reads=`ls reads | grep "1.fastq" | sed 's/_1.fastq//g'`
-mkdir taxa/paired_reads
+reads=`ls 1-reads | grep "1.fastq" | sed 's/_1.fastq//g'`
+mkdir 2-taxa/paired_reads
 for f in $reads
 do 
-	scripts/pear -f reads/"$f"_1.fastq -r reads/"$f"_2.fastq -o taxa/paired_reads/"$f"
-	rm -rf taxa/paired_reads/*discarded*
-	rm -rf taxa/paired_reads/*unassembled*
+	scripts/pear -f 1-reads/"$f"_1.fastq -r 1-reads/"$f"_2.fastq -o 2-taxa/paired_reads/"$f"
+	rm -rf 2-taxa/paired_reads/*discarded*
+	rm -rf 2-taxa/paired_reads/*unassembled*
 done
 
 module unload python
 module load python/3.6.8
 module load focus/1.4
-focus -q taxa/paired_reads -o taxa/focus_output --threads 1
+focus -q 2-taxa/paired_reads -o 2-taxa/focus_output --threads 1
